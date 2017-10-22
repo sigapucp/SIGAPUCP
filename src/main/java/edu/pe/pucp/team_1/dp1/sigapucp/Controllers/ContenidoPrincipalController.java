3db28ee58afc081d713f932dbd01a8450814cba8
Herbertvc <herbert.videla@tektonlabs.com>
2017-10-21 22:52:53 -0500

    Example of Navigation inside the Product Controller

--- src/main/java/edu/pe/pucp/team_1/dp1/sigapucp/Controllers/ContenidoPrincipalController.java
+++ src/main/java/edu/pe/pucp/team_1/dp1/sigapucp/Controllers/ContenidoPrincipalController.java
@@ -20,7 +20,8 @@ public class ContenidoPrincipalController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         NavegacionLateralController.abrirDetalle.addHandler((sender, args) -> {
             try {
-                AnchorPane contenido = FXMLLoader.load(getClass().getResource(args.getPathContenido()));
+                FXMLLoader loader = new FXMLLoader(getClass().getResource(args.getPathContenido()));
+                AnchorPane contenido = (AnchorPane) loader.load();
                 contenedor_modulos_contenido.getChildren().setAll(contenido);
             } catch (IOException ex) {
                 Logger.getLogger(ContenidoPrincipalController.class.getName()).log(Level.SEVERE, null, ex);
