bd222f68916c635d13b345aab300acfa1a430c79
Hugo <a20103656@pucp.pe>
2017-10-21 20:51:57 -0500

    adios vaquero

--- src/main/java/edu/pe/pucp/team_1/dp1/sigapucp/Controllers/AuditoriaController.java
+++ src/main/java/edu/pe/pucp/team_1/dp1/sigapucp/Controllers/AuditoriaController.java
@@ -7,10 +7,17 @@ package edu.pe.pucp.team_1.dp1.sigapucp.Controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import org.springframework.stereotype.Component;
 
@@ -23,27 +30,78 @@ import org.springframework.stereotype.Component;
 public class AuditoriaController implements Initializable {
 
     @FXML
-    private DatePicker AuditoriaFechaUno;
-    @FXML
     private DatePicker AuditoriaFechaDos;
+
     @FXML
-    private CheckBox EntreFecha;
+    private Button BtnBuscar;
+
     @FXML
     private TextField EmpleadoAuditoria;
-    @FXML
-    private TextField DescripcionAuditoria;
+
     @FXML
     private CheckBox EntreHora;
+
     @FXML
-    private TextField HoraUno;
+    private TableView<Auditoria> TablaAuditoria;
+
+    @FXML
+    private TableColumn<Auditoria,String> ColumnaEmpleado;
+
+    @FXML
+    private CheckBox EntreFecha;
+
     @FXML
     private TextField HoraDos;
 
+    @FXML
+    private DatePicker AuditoriaFechaUno;
+
+    @FXML
+    private TextField DescripcionAuditoria;
+
+    @FXML
+    private TextField HoraUno;
+    
+    private final ObservableList<Auditoria> masterData = FXCollections.observableArrayList();
+
+    public AuditoriaController(){
+        masterData.add(new Auditoria("8:00","21/10/2017","Hugo","Atiende","Cato","Hugo esta atendiendo"));
+        masterData.add(new Auditoria("8:00","21/10/2017","Joel","Dormir","Casa","Joel esta durmiendo"));
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO      
         EntreFecha.setOnAction(e -> handleCheckBoxUno(AuditoriaFechaDos,EntreFecha));
         EntreHora.setOnAction(e -> handleCheckBoxDos(HoraDos,EntreHora));
+           
+        ColumnaEmpleado.setCellValueFactory(cellData -> cellData.getValue().EmpleadoProperty());
+        ColumnaEmpleado.setCellValueFactory(cellData -> cellData.getValue().HoraProperty());
+        ColumnaEmpleado.setCellValueFactory(cellData -> cellData.getValue().FechaProperty());
+        ColumnaEmpleado.setCellValueFactory(cellData -> cellData.getValue().AccionProperty());
+        ColumnaEmpleado.setCellValueFactory(cellData -> cellData.getValue().ModuloProperty());
+        ColumnaEmpleado.setCellValueFactory(cellData -> cellData.getValue().DescripcionProperty());
+        
+        FilteredList<Auditoria> filteredData = new FilteredList<>(masterData, p -> true);
+        
+        EmpleadoAuditoria.textProperty().addListener((observable, oldValue, newValue) -> {
+            filteredData.setPredicate(auditoria -> {
+                if (newValue == null || newValue.isEmpty()){
+                    return true;
+                }
+                String lowerCaseFilter = newValue.toLowerCase();
+                if (auditoria.getEmpleado().toLowerCase().indexOf(lowerCaseFilter) != -1){
+                    return true;
+                }             
+                return false;
+            });
+        });
+        
+        
+        SortedList<Auditoria> sortedData = new SortedList<>(filteredData);
+        sortedData.comparatorProperty().bind(TablaAuditoria.comparatorProperty());
+        
+        TablaAuditoria.setItems(sortedData);
+               
     } 
     
     private void handleCheckBoxUno(DatePicker AuditoriaFechaDos,CheckBox EntreFecha){
