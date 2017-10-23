88453661d6b178d09fed6cbe683d8a038bdc3a88
Herbertvc <herbert.videla@tektonlabs.com>
2017-10-23 14:42:16 -0500

    Navigation with two levels is now up and running

---
+++ src/main/java/edu/pe/pucp/team_1/dp1/sigapucp/Controllers/BotonesAccionesController.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.pe.pucp.team_1.dp1.sigapucp.Controllers;
+
+import edu.pe.pucp.team_1.dp1.sigapucp.CustomEvents.Event;
+import edu.pe.pucp.team_1.dp1.sigapucp.CustomEvents.IEvent;
+import edu.pe.pucp.team_1.dp1.sigapucp.Navegacion.ejecutarAccionArgs;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ *
+ * @author herbert
+ */
+public class BotonesAccionesController extends Controller {
+
+    private IEvent<ejecutarAccionArgs> ejecutarAccionController;
+    
+    @FXML
+    public void actionHumo(ActionEvent event) {
+        //PRoceso
+        //activeController.save(/**/);
+        ejecutarAccionArgs args = new ejecutarAccionArgs();
+        Button accionButton = (Button) event.getSource();
+        String accion = accionButton.getText();
+        args.setAccion(accion);
+        ejecutarAccionController.fire(this, args);
+    }
+    
+
+    @Override
+    public IEvent<ejecutarAccionArgs> getActionEvent() {
+        return ejecutarAccionController;
+    }
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        ejecutarAccionController = new Event<>();
+    }
+    
+}
---
+++ src/main/java/edu/pe/pucp/team_1/dp1/sigapucp/Navegacion/ejecutarAccionArgs.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.pe.pucp.team_1.dp1.sigapucp.Navegacion;
+
+import edu.pe.pucp.team_1.dp1.sigapucp.CustomEvents.EventArgs;
+
+/**
+ *
+ * @author herbert
+ */
+public class ejecutarAccionArgs extends EventArgs {
+    private String accion;
+    
+    public String getAccion() {
+        return accion;
+    }
+    
+    public void setAccion(String accion) {
+        this.accion = accion;
+    }
+    
+}
