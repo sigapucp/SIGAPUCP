38082f4551e3d8c1a034913f9457fa1c9352afe0
Jaume Jacint Bofi Reátegui <jaume.bofi@gmail.com>
2017-10-28 21:58:34 -0500

    Final Usuarios y Roles

--- src/main/java/edu/pe/pucp/team_1/dp1/sigapucp/Controllers/RecursosHumanos/RolesController.java
+++ src/main/java/edu/pe/pucp/team_1/dp1/sigapucp/Controllers/RecursosHumanos/RolesController.java
@@ -16,11 +16,13 @@ import edu.pe.pucp.team_1.dp1.sigapucp.Models.RecursosHumanos.Rol;
 import edu.pe.pucp.team_1.dp1.sigapucp.Models.RecursosHumanos.Usuario;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import static java.util.Locale.filter;
 import java.util.ResourceBundle;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.value.ChangeListener;
@@ -36,16 +38,13 @@ import javafx.geometry.Side;
 import javafx.scene.control.CheckBoxTreeItem;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TitledPane;
 import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeTableCell;
 import javafx.scene.control.TreeTableColumn;
 import javafx.scene.control.TreeTableView;
-import javafx.scene.control.cell.CheckBoxTreeCell;
 import javafx.scene.control.cell.CheckBoxTreeTableCell;
 import javafx.scene.layout.AnchorPane;
 import javafx.util.Callback;
@@ -123,6 +122,7 @@ public class RolesController extends Controller{
     @FXML
     private void visualizarRol(ActionEvent event) {
         crearNuevo = false;
+        rolSelecionado = null;
         rolSelecionado = TablaRoles.getSelectionModel().getSelectedItem();
         if(rolSelecionado == null) return;        
         setRolVisible(rolSelecionado);  
@@ -130,19 +130,74 @@ public class RolesController extends Controller{
 
     @FXML
     private void buscarRoles(ActionEvent event) {
+        
+        String codigo = BusquedaRol.getSelectionModel().getSelectedItem();
+        String nombre = BusquedaNombre.getText();
+        String estado = BusquedaEstado.getSelectionModel().getSelectedItem();        
+        
+        tempRoles = Rol.findAll();
+        
+        if(codigo!=null&&!codigo.isEmpty())
+        {            
+            tempRoles = tempRoles.stream().filter(p -> p.getString("rol_cod").equals(codigo)).collect(Collectors.toList());
+        }
+        
+        if(nombre!=null&&!nombre.isEmpty())
+        {
+            tempRoles = tempRoles.stream().filter(p -> p.getString("nombre").equals(nombre)).collect(Collectors.toList());
+        }
+        
+        if(estado!=null&&!estado.isEmpty())
+        {
+            tempRoles = tempRoles.stream().filter(p -> p.getString("estado").equals(estado)).collect(Collectors.toList());
+        }                
+        RefrescarTabla(tempRoles);
+        try {                        
+        } catch (Exception e) {
+            
+        }
     }
     
      private void limpiarVerRol()
     {
         VerNombre.clear();
         VerDescripcion.clear();
-        ArbolPrivilegiosTabla.setRoot(null);          
+        for(PrivilegioEntrada privilegio:privilegiosEntrada)
+        {
+            privilegio.setEstadoPrivilegio(false);
+        }
+        if(rootNode!=null) rootNode.getValue().setEstadoPrivilegio(false);
     }    
+     
+     @Override
+     public void desactivar()
+     {
+        if(rolSelecionado==null) 
+        {
+            infoController.show("No ha seleccionado un rol");            
+            return;
+        }
+        try {
+           Base.openTransaction();
+           
+           rolSelecionado.set("estado",Rol.ESTADO.INACTIVO.name());
+           rolSelecionado.saveIt();
+
+           Base.commitTransaction();
+        } catch (Exception e) {
+           Base.rollbackTransaction();
+        }
+     }
     
     @Override
     public void nuevo()
     {
         crearNuevo = true;
+        if(ArbolPrivilegiosTabla.getRoot()==null)
+        {
+            ArbolPrivilegiosTabla.setRoot(rootNode);                                                          
+        }
+        
         limpiarVerRol();
     }
     
@@ -151,13 +206,71 @@ public class RolesController extends Controller{
     {        
         if(crearNuevo)
         {
-//            crearUsuario();            
+            crearRol();            
         }else
         {
-            if(rolSelecionado == null) return;
+            if(rolSelecionado==null) 
+            {
+                infoController.show("No ha seleccionado un rol");
+                return;
+            }
             editarRol(rolSelecionado);
         }                
-          RefrescarTabla();
+        RefrescarTabla(Rol.findAll());
+    }
+    
+     private void crearRol()
+    {
+        rolSelecionado = null;
+        String nombre = VerNombre.getText();
+        String descripcion = VerDescripcion.getText();        
+        
+        String cod = "ROL" + String.valueOf(Integer.valueOf(String.valueOf((Base.firstCell("select last_value from roles_rol_id_seq")))) + 1);  
+        if(!confirmatonController.show("Se creara el Rol con codigo: " + cod, "¿Desea continuar?")) return;
+        
+        try{      
+        Base.openTransaction();    
+        
+        Rol rol = new Rol();
+        rol.set("rol_cod",cod);
+        rol.set("nombre",nombre);
+        rol.set("descripcion",descripcion);         
+        rol.set("estado",Rol.ESTADO.ACTIVO.name());
+        rol.set("last_user_change",usuarioActual.getString("usuario_cod"));
+        
+        rol.saveIt();
+        
+        for(PrivilegioEntrada privilegio:privilegiosEntrada)
+        {
+            if(privilegio.getEstadoPrivilegio().getValue())
+            {
+                AccionxRol accionxRol = new AccionxRol();
+                accionxRol.set("menu_id",privilegio.getIdMenu());
+                accionxRol.set("rol_id",rol.getId());
+                accionxRol.set("rol_cod",cod);                
+                accionxRol.set("solo_admin","F");
+                
+                if(privilegio.getTipo() == PrivilegioEntrada.TIPOPRIVILEGIO.ACCION)
+                {
+                    accionxRol.set("accion_id",privilegio.getId());
+                    accionxRol.set("accion_cod",privilegio.getCod());                    
+                }else
+                {
+                    accionxRol.set("accion_id",Accion.ACCION.getId(Accion.ACCION.VIW));
+                    accionxRol.set("accion_cod",Accion.ACCION.VIW.name());                    
+                }                
+                accionxRol.saveIt();
+            }
+        }          
+        
+        Base.commitTransaction();
+        infoController.show("El rol ha sido editado creado satisfactoriamente");        
+        }
+        catch(Exception e){
+           Base.rollbackTransaction();           
+        }finally{
+           crearNuevo = false; 
+        }        
     }
     
      private void editarRol(Rol rol)
@@ -170,40 +283,48 @@ public class RolesController extends Controller{
         
         try{      
         Base.openTransaction();       
-        rol.set("nombre",nombre);
-        rol.set("descripcion", descripcion);
+        rol.set("nombre",nombre);     
+        rol.set("descripcion", descripcion);        
+        rol.set("last_user_change",usuarioActual.getString("usuario_cod"));
         
         AccionxRol.delete("rol_id = ?", rol.getId());
         
         for(PrivilegioEntrada privilegio:privilegiosEntrada)
         {
-            if(privilegio.getTipo() == PrivilegioEntrada.TIPOPRIVILEGIO.ACCION&&privilegio.getEstadoPrivilegio().getValue())
+            if(privilegio.getEstadoPrivilegio().getValue())
             {
                 AccionxRol accionxRol = new AccionxRol();
                 accionxRol.set("menu_id",privilegio.getIdMenu());
                 accionxRol.set("rol_id",rol.getId());
-                accionxRol.set("rol_cod",cod);
-                accionxRol.set("accion_id",privilegio.getId());
-                accionxRol.set("accion_cod",privilegio.getCod());
+                accionxRol.set("rol_cod",cod);                
                 accionxRol.set("solo_admin","F");
+                
+                if(privilegio.getTipo() == PrivilegioEntrada.TIPOPRIVILEGIO.ACCION)
+                {
+                    accionxRol.set("accion_id",privilegio.getId());
+                    accionxRol.set("accion_cod",privilegio.getCod());                    
+                }else
+                {
+                    accionxRol.set("accion_id",Accion.ACCION.getId(Accion.ACCION.VIW));
+                    accionxRol.set("accion_cod",Accion.ACCION.VIW.name());                    
+                }                
                 accionxRol.saveIt();
             }
         }                
         rol.saveIt();
-        Base.commitTransaction();
-        
-        infoController.show("El usuario ha sido editado satisfactoriamente");        
+        Base.commitTransaction();        
+        infoController.show("El Rol ha sido editado satisfactoriamente");        
         }
         catch(Exception e){
            Base.rollbackTransaction();
         }                
     }
     
-    private void RefrescarTabla()
+    private void RefrescarTabla(List<Rol> tempRoles)
     {
         try {
             roles.removeAll(roles);
-            tempRoles = Usuario.findAll();               
+            if(tempRoles == null) return;
             for (Rol rol : tempRoles) {
                 roles.add(rol);
             }               
@@ -227,20 +348,19 @@ public class RolesController extends Controller{
             }
             modificarArbolPrivilegios(rol);
             
-        } catch (Exception e) {
-            int a = 5;            
+        } catch (Exception e) {         
         }                        
     }
     
     public void modificarArbolPrivilegios(Rol rol)
     {
-        List<AccionxRol> accionesxroles = AccionxRol.where("rol_id = ?", rol.getId());
+        List<AccionxRol> accionesxroles = AccionxRol.where("rol_id = ?", rol.getId());        
+        
         for(PrivilegioEntrada privilegio:privilegiosEntrada)
         {
             if(privilegio.getTipo() == PrivilegioEntrada.TIPOPRIVILEGIO.ACCION)
             {
-                AccionxRol accionxrol = AccionxRol.findFirst("menu_id = ? AND accion_id = ? AND rol_id = ?",privilegio.getIdMenu(),privilegio.getId(),rol.getId());
-                if(accionxrol!=null)
+                if(accionesxroles.stream().anyMatch(o -> o.getInteger("accion_id") == privilegio.getId() && o.getInteger("menu_id") == privilegio.getIdMenu()))
                 {
                     privilegio.setEstadoPrivilegio(true);
                 }else
@@ -249,8 +369,7 @@ public class RolesController extends Controller{
                 }
             }else
             {
-                AccionxRol accionxrol = AccionxRol.findFirst("menu_id = ?  AND rol_id = ?",privilegio.getId(),rol.getId());
-                if(accionxrol!=null)
+                if(accionesxroles.stream().anyMatch(o -> o.getInteger("menu_id") == privilegio.getId()))
                 {
                     privilegio.setEstadoPrivilegio(true);
                 }else
@@ -276,12 +395,14 @@ public class RolesController extends Controller{
         {
             List<Accion> acciones = menu.getAll(Accion.class);
             PrivilegioEntrada menuEntrada = new PrivilegioEntrada((int)menu.getId(),"", menu.getString("nombre"), false, PrivilegioEntrada.TIPOPRIVILEGIO.MENU);
+            menuEntrada.setIdMenu((int)menu.getId());
             privilegiosEntrada.add(menuEntrada);
             CheckBoxTreeItem<PrivilegioEntrada> menuNode = new CheckBoxTreeItem<>(menuEntrada);                
             rootNode.getChildren().add(menuNode);                               
 
             for(Accion accion:acciones)
             {
+                if(accion.getInteger("accion_id")== Accion.ACCION.getId(Accion.ACCION.VIW)) continue;
                 PrivilegioEntrada accionEntrada = new PrivilegioEntrada((int)accion.getId(),accion.getString("accion_cod"), accion.getString("nombre"), false, PrivilegioEntrada.TIPOPRIVILEGIO.ACCION);
                 accionEntrada.setIdMenu((int)menu.getId());
                 privilegiosEntrada.add(accionEntrada);
@@ -289,6 +410,12 @@ public class RolesController extends Controller{
                 menuNode.getChildren().add(accionNode);                    
             }                
         }        
+    }
+    
+    @Override
+    public Menu.MENU getMenu()
+    {
+        return Menu.MENU.Roles;
     }
     
     @Override
@@ -300,37 +427,46 @@ public class RolesController extends Controller{
             ColumnaEstado.setCellValueFactory((TableColumn.CellDataFeatures<Rol, String> p) -> new ReadOnlyObjectWrapper(p.getValue().get("estado")));             
             ColumnaDescipcion.setCellValueFactory((TableColumn.CellDataFeatures<Rol, String> p) -> new ReadOnlyObjectWrapper(p.getValue().get("descripcion")));             
             
-            ArbolAccionesColumna.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<PrivilegioEntrada, String>, ObservableValue<String>>() {     
-            @Override
-            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<PrivilegioEntrada, String> param) {
-                return param.getValue().getValue().getNombrePrivilegio();
-            }                       
-            });
+            ArbolAccionesColumna.setCellValueFactory((TreeTableColumn.CellDataFeatures<PrivilegioEntrada, String> param) -> param.getValue().getValue().getNombrePrivilegio());
 
-            ArbolPrivilegiosColumna.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<PrivilegioEntrada, Boolean>, ObservableValue<Boolean>>() {     
-            @Override
-            public ObservableValue<Boolean> call(TreeTableColumn.CellDataFeatures<PrivilegioEntrada, Boolean> param) {
+            ArbolPrivilegiosColumna.setCellValueFactory((TreeTableColumn.CellDataFeatures<PrivilegioEntrada, Boolean> param) -> {
                 TreeItem<PrivilegioEntrada> treeItem = param.getValue();
                 SimpleBooleanProperty booleanProp = treeItem.getValue().getEstadoPrivilegio();
                 booleanProp.addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
                     
-                if(treeItem.getValue().getTipo() == PrivilegioEntrada.TIPOPRIVILEGIO.MENU)
-                {
-                    ObservableList<TreeItem<PrivilegioEntrada>> children = treeItem.getChildren(); 
-                    if(newValue) return;
-                    for(TreeItem<PrivilegioEntrada> child:children)
-                    {                      
-                       child.getValue().setEstadoPrivilegio(false);
-                    }                          
-                }else
-                {                    
-                    TreeItem<PrivilegioEntrada> parent = treeItem.getParent();
-                    if(!newValue) return;  
-                    parent.getValue().setEstadoPrivilegio(true);
-                }
-            });                                             
+                  
+                    
+                    if(treeItem.getValue().getTipo() == PrivilegioEntrada.TIPOPRIVILEGIO.MENU)
+                    {
+                        if(treeItem.getValue().getId() == -1)
+                        {
+                            ObservableList<TreeItem<PrivilegioEntrada>> children = treeItem.getChildren();
+                            for(TreeItem<PrivilegioEntrada> child:children)
+                            {
+                                child.getValue().setEstadoPrivilegio(true);
+                                ObservableList<TreeItem<PrivilegioEntrada>> moreChildren = child.getChildren();
+                                for(TreeItem<PrivilegioEntrada> morechild:moreChildren)
+                                {
+                                    morechild.getValue().setEstadoPrivilegio(true);
+                                }                                
+                            }                       
+                        }else
+                        {
+                            ObservableList<TreeItem<PrivilegioEntrada>> children = treeItem.getChildren();
+                            if(newValue) return;
+                            for(TreeItem<PrivilegioEntrada> child:children)
+                            {
+                                child.getValue().setEstadoPrivilegio(false);
+                            }                            
+                        }                        
+                    }else
+                    {
+                        TreeItem<PrivilegioEntrada> parent = treeItem.getParent();
+                        if(!newValue) return;
+                        parent.getValue().setEstadoPrivilegio(true);
+                    }
+                });                                             
                 return booleanProp;
-            }                       
             });
                                             
             ArbolPrivilegiosColumna.setCellFactory((TreeTableColumn<PrivilegioEntrada, Boolean> param) -> {
@@ -343,11 +479,8 @@ public class RolesController extends Controller{
             ObservableList<String> estadosNoPad = FXCollections.observableArrayList(); 
             
             estados.add("");
-            estados.add("ACTIVO");
-            estados.add("INACTIVO");
-            estadosNoPad.add("ACTIVO");
-            estadosNoPad.add("INACTIVO");
-            
+            estados.addAll(Arrays.asList(Rol.ESTADO.values()).stream().map(x->x.name()).collect(Collectors.toList()));            
+            estadosNoPad.addAll(Arrays.asList(Rol.ESTADO.values()).stream().map(x->x.name()).collect(Collectors.toList()));                              
             
             ObservableList<String> rolesNames = FXCollections.observableArrayList();   
             ObservableList<String> rolesCods = FXCollections.observableArrayList();   
@@ -364,8 +497,7 @@ public class RolesController extends Controller{
             BusquedaEstado.setItems(estados);
             BusquedaRol.setItems(rolesNames);
                                    
-            TablaRoles.setItems(roles);
-            ArbolPrivilegiosTabla.setShowRoot(false);
+            TablaRoles.setItems(roles);           
             initializarArbolPrivilegios();
          
            
--- src/main/java/edu/pe/pucp/team_1/dp1/sigapucp/Models/RecursosHumanos/Menu.java
+++ src/main/java/edu/pe/pucp/team_1/dp1/sigapucp/Models/RecursosHumanos/Menu.java
@@ -18,5 +18,43 @@ import org.javalite.activejdbc.annotations.Table;
 @IdName("menu_id")
 @Many2Many(other = Accion.class, join = "AccionesxMenus", sourceFKName = "menu_id", targetFKName = "accion_id")
 public class Menu extends Model{
-    
+    public enum MENU
+    {
+        NullMenu,
+        Dashboard,
+        Auditoria,
+        ReportedeErrorres,
+        Almacenes,
+        Categorias,
+        Productos,
+        Racks,
+        OrdendeEntrada,
+        OrdendeDespacho,
+        Clientes,
+        Proformas,
+        Pedidos,
+        Proveedores,
+        Promociones,
+        Fletes,
+        DocumentosdeVenta,
+        NotasdeCredito,
+        Usuarios,
+        Roles,
+        Login,
+        Simulacion;
+
+        public static int getId(MENU menu)
+        {
+            return menu.ordinal() + 1;
+        }
+        
+        public static MENU getMenu(int id)
+        {
+            for(MENU menu: values())
+            {
+                if(getId(menu) == id) return menu;
+            }
+            return NullMenu;
+        }
+    }
 }
