23bf822ded49c6263e0a7cc934929e531d9f4584
Gustavo Galarreta <gustavo.galarreta@pucp.pe>
2017-10-28 19:35:58 -0500

    productos con unidades

--- src/main/java/edu/pe/pucp/team_1/dp1/sigapucp/Models/Materiales/TipoProducto.java
+++ src/main/java/edu/pe/pucp/team_1/dp1/sigapucp/Models/Materiales/TipoProducto.java
@@ -27,73 +27,18 @@ import org.javalite.activejdbc.annotations.Table;
 @IdName("tipo_id")
 public class TipoProducto extends Model{
         
-    
-    
-    public TipoProducto() {}
 
-    public TipoProducto(StringProperty tipo_cod, Integer tipo_id, float peso, StringProperty nombre, char perecible, String descripcion, 
-            String LastUserChange, Date LastDateChange, char FlagLastOperation, String estado, float longitud, float ancho,
-            Integer unidad_id) {
+    public void asignar_atributos(String usuario, String tipo_cod, float peso, String nombre, char perecible, String descripcion, 
+            float longitud, float ancho, String unidad_id) {
         set("tipo_cod",tipo_cod);
-        set("tipo_id",tipo_id);
         set("peso",peso);
         set("nombre",nombre);
-        set("perecible",perecible);
+        set("pericible",perecible);
         set("descripcion",descripcion);
-        set("last_user_change",LastUserChange);
-        set("last_date_change",LastDateChange);
-        set("flag_last_operation",FlagLastOperation);
-        set("estado",estado);
+        set("estado","activo");
         set("longitud",longitud);
         set("ancho",ancho);
-        set("unidad_id",unidad_id);
+        set("last_user_change",usuario);
+        set("unidad_id", Integer.parseInt(unidad_id));
     }
-    /*public static List<TipoProducto> inicializarProductos (){
-        Base.open();
-        TipoProducto productosLista = TipoProducto.where("tipo_cod = 'lapipilot'");
-        System.out.println(productosLista);
-
-
-        //
-        //Boolean hayProductos = false;
-        /*try{
-            Base.open();
-            productosLista = TipoProducto.where("tipo_cod = 'lapipilot'");
-
-            Base.close();
-        }
-        catch (Exception e){
-            System.out.println(e);
-        }*
-        return productosLista;
-    }*/
-    
- 
-   /* 
-    public static List<TipoProducto> recuperarRegistros(){
-        
-        List<TipoProducto> registros = TipoProducto.findAll();
-        return registros;
-    }*/
-    public static String hallarNombreTipoProducto(){       
-        Base.open();
-        TipoProducto tipo = TipoProducto.findById(1);
-        String nombre = tipo.getTipoProd();     
-        return nombre;
-    }
-    public static String hallarCodigoTipoProducto(){       
-        Base.open();
-        TipoProducto tipo = TipoProducto.findById(1);
-        String codigo = tipo.getTipoCod();     
-        return codigo;
-    }
-    public String getTipoProd(){     
-        return getString("nombre");
-    }
-       
-    public String getTipoCod(){        
-        return getString("tipo_cod");
-    }
-    
-
 }
