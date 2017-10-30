87ef352891360faa88f50e7a351ed0e1abc8b3c9
Hugo <a20103656@pucp.pe>
2017-10-29 23:28:01 -0500

    kardex preemiliminar

---
+++ src/main/java/edu/pe/pucp/team_1/dp1/sigapucp/Controllers/Ventas/KardexController.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.pe.pucp.team_1.dp1.sigapucp.Controllers.Ventas;
+import edu.pe.pucp.team_1.dp1.sigapucp.Models.Materiales.TipoProducto;
+import edu.pe.pucp.team_1.dp1.sigapucp.Controllers.Controller;
+import edu.pe.pucp.team_1.dp1.sigapucp.Controllers.Seguridad.InformationAlertController;
+import edu.pe.pucp.team_1.dp1.sigapucp.Models.Materiales.CategoriaProducto;
+import edu.pe.pucp.team_1.dp1.sigapucp.Models.Materiales.TipoProducto;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import org.javalite.activejdbc.Base;
+
+/**
+ *
+ * @author Hugo
+ */
+public class KardexController extends Controller {
+    
+    @FXML
+    private AnchorPane kardexProducto;
+    
+    @FXML
+    private TableView<TipoProducto> tablaProductos; 
+    
+    @FXML
+    private TableColumn<TipoProducto,String> ColumnaCodigoProducto;
+
+    @FXML
+    private TableColumn<TipoProducto,String> ColumnaTipoProducto;
+     
+    @FXML
+    private TextField tipoProductoBuscar;  
+    @FXML
+    private TextField codigoProductoBuscar;    
+    
+    
+    private List<TipoProducto> tipos;
+    private final ObservableList<TipoProducto> masterData = FXCollections.observableArrayList();
+    private InformationAlertController infoController;
+    private TipoProducto producto_seleccionado ;
+    
+    public void inhabilitar_formulario(){
+        kardexProducto.setDisable(true);
+    }
+    
+    public void habilitar_formulario(){
+        kardexProducto.setDisable(false);
+    }
+    
+    private void cargar_tabla_index(){
+        tablaProductos.getItems().clear();
+        masterData.clear();
+        for (TipoProducto tipo : tipos){
+            masterData.add(tipo);
+        }
+        tablaProductos.setEditable(false);
+        ColumnaTipoProducto.setCellValueFactory((TableColumn.CellDataFeatures<TipoProducto, String> p) -> new ReadOnlyObjectWrapper(p.getValue().get("tipo_cod")));
+        ColumnaCodigoProducto.setCellValueFactory((TableColumn.CellDataFeatures<TipoProducto, String> p) -> new ReadOnlyObjectWrapper(p.getValue().get("nombre")));
+        tablaProductos.getItems().addAll(masterData);
+        
+    }
+    
+    
+    
+    public KardexController(){
+        Base.open("org.postgresql.Driver", "jdbc:postgresql://200.16.7.146/sigapucp_db_admin", "sigapucp", "sigapucp");       
+        producto_seleccionado = null;
+        infoController = new InformationAlertController();
+        
+    }
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        inhabilitar_formulario();
+        tipos = null;
+        tipos = TipoProducto.findAll();
+        cargar_tabla_index();
+    }    
+}
