f09aaaed22551bea7301819f0320588cbfb213e9
Gustavo Galarreta <gustavo.galarreta@pucp.pe>
2017-10-30 17:11:22 -0500

    kardex vista

--- src/main/java/edu/pe/pucp/team_1/dp1/sigapucp/Controllers/General/Kardex/KardexController.java
+++ src/main/java/edu/pe/pucp/team_1/dp1/sigapucp/Controllers/General/Kardex/KardexController.java
@@ -9,12 +9,16 @@ import edu.pe.pucp.team_1.dp1.sigapucp.Controllers.Controller;
 import edu.pe.pucp.team_1.dp1.sigapucp.Controllers.Seguridad.InformationAlertController;
 import edu.pe.pucp.team_1.dp1.sigapucp.Models.Materiales.CategoriaProducto;
 import edu.pe.pucp.team_1.dp1.sigapucp.Models.Materiales.TipoProducto;
+import java.io.IOException;
+import java.util.stream.Collectors;
+
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -44,6 +48,12 @@ public class KardexController extends Controller {
     private TextField tipoProductoBuscar;  
     @FXML
     private TextField codigoProductoBuscar;    
+    @FXML
+    private TextField producto_nombre;
+    @FXML
+    private TextField producto_codigo;
+    @FXML
+    private TextField producto_categoria;
     
     
     private List<TipoProducto> tipos;
@@ -72,7 +82,43 @@ public class KardexController extends Controller {
         
     }
     
+    @FXML
+    public void mostrar_detalle_tipo_producto(ActionEvent event) throws IOException{
+        try{
+            TipoProducto registro_seleccionado = tablaProductos.getSelectionModel().getSelectedItem();
+            kardexProducto.setDisable(false);
+            producto_nombre.setText(registro_seleccionado.getString("nombre"));
+            producto_codigo.setText(registro_seleccionado.getString("tipo_cod"));
+            //TODO
+        }
+        catch( Exception e){
+            System.out.println(e);
+        }        
+    }  
     
+    @FXML
+    public void buscar_producto(ActionEvent event)throws IOException{
+        String codigo_producto = codigoProductoBuscar.getText();
+        String nombre_producto = tipoProductoBuscar.getText();
+        List<TipoProducto> temp_productos = TipoProducto.findAll();
+        
+        if(codigo_producto!=null&&!codigo_producto.isEmpty())
+        {            
+            temp_productos = temp_productos.stream().filter(p -> p.getString("tipo_cod").equals(codigo_producto)).collect(Collectors.toList());
+        }
+
+        if(nombre_producto!=null&&!nombre_producto.isEmpty())
+        {            
+            temp_productos = temp_productos.stream().filter(p -> p.getString("nombre").equals(nombre_producto)).collect(Collectors.toList());
+        }
+                   
+        tipos = temp_productos;
+        cargar_tabla_index();
+        try {                        
+        } catch (Exception e) {
+            infoController.show("El producto contiene errores : " + e);                    
+        }        
+    }
     
     public KardexController(){
         Base.open("org.postgresql.Driver", "jdbc:postgresql://200.16.7.146/sigapucp_db_admin", "sigapucp", "sigapucp");       
@@ -87,5 +133,11 @@ public class KardexController extends Controller {
         tipos = null;
         tipos = TipoProducto.findAll();
         cargar_tabla_index();
+        tablaProductos.getSelectionModel().selectedIndexProperty().addListener((obs,oldSelection,newSelection) -> {
+            if (newSelection != null){
+                producto_seleccionado = tablaProductos.getSelectionModel().getSelectedItem();                
+                tablaProductos.getSelectionModel().clearSelection();        
+            }
+        });        
     }    
 }
