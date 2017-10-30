5becb1f0bdfe56deb0181358ecde4d9bbff6a0c5
Joel Aguirre <aguirre.joel@pucp.pe>
2017-10-30 02:22:43 -0500

    pedidos - crear pedido sin productos desde cero. No a partir de proforma

--- src/main/java/edu/pe/pucp/team_1/dp1/sigapucp/Controllers/Ventas/PedidosController.java
+++ src/main/java/edu/pe/pucp/team_1/dp1/sigapucp/Controllers/Ventas/PedidosController.java
@@ -6,11 +6,21 @@
 package edu.pe.pucp.team_1.dp1.sigapucp.Controllers.Ventas;
 
 import edu.pe.pucp.team_1.dp1.sigapucp.Controllers.Controller;
+import edu.pe.pucp.team_1.dp1.sigapucp.Controllers.Seguridad.InformationAlertController;
+import edu.pe.pucp.team_1.dp1.sigapucp.Models.Ventas.OrdenCompra;
 import java.io.IOException;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.CheckBox;
@@ -22,7 +32,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -38,13 +51,37 @@ public class PedidosController extends Controller {
     @FXML
     private AnchorPane pedidoContainer;
     @FXML
+    private TextField subTotal;
+    @FXML
+    private TextField igvPedido;
+    @FXML
+    private TextField totalPedido;
+    @FXML
+    private TextField clienteSearch;
+    @FXML
+    private ComboBox<String> estadoSearch;
+    @FXML
+    private TextField pedidoSearch;
+    @FXML
+    private TableView<OrdenCompra> tablaPedidos;
+    @FXML
+    private AnchorPane pedidoForm;
+    @FXML
     private RadioButton tipoDocBoleta;
     @FXML
     private RadioButton tipoDocFactura;
     @FXML
+    private TextField clienteSh;
+    @FXML
+    private TextField vendedorSh;
+    @FXML
     private TextField ruc;
     @FXML
     private TextField dni;
+    @FXML
+    private DatePicker fechaPed;
+    @FXML
+    private TextField tipoCambio;
     @FXML
     private TextField envioDir;
     @FXML
@@ -52,31 +89,80 @@ public class PedidosController extends Controller {
     @FXML
     private CheckBox mismaDir;
     @FXML
+    private ComboBox<String> moneda;
+    @FXML
+    private AnchorPane pedidoTable;
+    @FXML
     private Spinner<?> cantProd;
     @FXML
-    private TextField clienteSh;
+    private TextField producto;
     @FXML
-    private DatePicker fechaPed;
-        
-    @FXML static Stage modal_stage = new Stage();
+    private Button agregarProducto;
+    @FXML
+    static Stage modal_stage = new Stage();
     
-    @FXML private ProformasController proformasController;
+    boolean crearNuevo;
+    
+    private final ObservableList<OrdenCompra> pedidos = FXCollections.observableArrayList();
+    
+    private List<OrdenCompra> tempPedidos;
+    
+    private InformationAlertController infoController;
+    
+    private OrdenCompra pedidoSeleccionado;
+    
+    
 
     /**
      * Initializes the controller class.
      */
+    
+    public PedidosController(){
+        if (!Base.hasConnection()) Base.open("org.postgresql.Driver", "jdbc:postgresql://200.16.7.146/sigapucp_db_admin", "sigapucp", "sigapucp");
+        tempPedidos = OrdenCompra.findAll();
+        for (OrdenCompra pedido : tempPedidos){
+            pedidos.add(pedido);
+        }
+        infoController = new InformationAlertController();
+        pedidoSeleccionado = null;
+        crearNuevo = false;
+    }
+    
+    @Override
+    public void nuevo(){
+        crearNuevo = true;
+        habilitar_formulario();
+    }
+    
+    @Override
+    public void guardar(){
+        if (crearNuevo){
+            crearPedido();
+        } else {
+            if (pedidoSeleccionado == null) return;
+            editarPedido(pedidoSeleccionado);
+        }
+        refrescarTabla();
+    }
+    
+    @FXML
+    void buscarPedido(ActionEvent event) {
+
+    }
+
+    @FXML
+    void visualizarPedido(ActionEvent event) {
+
+    }    
+    
     @Override
     @FXML
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        Base.close();
-        Base.open();
         mismaDir.setOnAction(e -> manejoTextoChckBox(factDir,mismaDir));
         tipoDocBoleta.setOnAction(e -> manejoTextoRadBttn1());
         tipoDocFactura.setOnAction(e -> manejoTextoRadBttn2());
-        if (proformasController != null){
-            this.clienteSh.setText(this.proformasController.clienteSh.getText());
-        }
+        inhabilitar_formulario();
         //Seteo la modal de agregar producto
         Parent modal_content;
         try {
@@ -91,10 +177,7 @@ public class PedidosController extends Controller {
         }
 
     }    
-    
-    public void injectController(ProformasController controller){
-        this.proformasController = controller;
-    }
+
     
     private void manejoTextoChckBox(TextField texto, CheckBox seleccionado){
         if (seleccionado.isSelected()) {
@@ -119,6 +202,78 @@ public class PedidosController extends Controller {
     @FXML
     private void handleAgregarProducto(ActionEvent event) throws IOException{
         modal_stage.showAndWait();
+    }
+
+    private void habilitar_formulario() {
+        pedidoForm.setDisable(false);
+        pedidoTable.setDisable(false);
+    }
+
+    private void crearPedido() {
+        //extraemos la informacion del formulario:
+        String cliente = clienteSh.getText();
+        //cliente id--- se busca por nombre
+        Integer clienteId = 1;
+        String vendedor = vendedorSh.getText();
+        LocalDate fechaLocal = fechaPed.getValue();
+        Date fecha = Date.from(fechaLocal.atStartOfDay(ZoneId.systemDefault()).toInstant());
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        String fechaEmision = dateFormat.format(fecha);
+        Double subtotal = Double.valueOf(subTotal.getText());
+        Integer cantProductos = (Integer) cantProd.getValue();
+        String orden_compra_cod = "ORD";
+        int monedaId;
+        if (moneda.getValue().toString().equals("SOLES")){
+             monedaId = 1;
+        } else {
+            monedaId = 2;
+        }
+        String usuario = "admin"; 
+        char flag = '1';
+        Integer usuario_id = 1;
+        Integer proforma = 1;
+        asignar_data(orden_compra_cod, clienteId, fechaEmision, subtotal, usuario, flag,usuario, usuario_id , monedaId, proforma);
+    }
+
+    
+    private void asignar_data(String orden_compra_cod, Integer clienteId, String fechaEmision, Double subtotal, String usuario, char flag, String usuario_cod, Integer usuario_id, Integer monedaId, Integer cotizacion_id){
+        try{      
+            Base.openTransaction();  
+            Integer cod = Integer.valueOf(String.valueOf((Base.firstCell("select last_value from ordenescompra_orden_compra_id_seq")))) + 1;        
+            orden_compra_cod = orden_compra_cod + Integer.toString(cod);
+            OrdenCompra pedido = new OrdenCompra();
+            pedido.set("orden_compra_cod",orden_compra_cod);
+            pedido.set("client_id",clienteId);
+            pedido.setDate("fecha_emision", fechaEmision);
+            pedido.set("subtotal", subtotal);
+            //pedido.set("estado", estado);
+            //proforma.set("last_user_change",usuarioActual.getString("cotizacion_cod"));
+            pedido.set("last_user_change", usuario);
+            pedido.set("flag_last_operation", flag);
+            pedido.set("moneda_id", monedaId);
+            pedido.saveIt();
+            Base.commitTransaction();
+            System.out.println("Todo Correcto BD");
+            infoController.show("Se ha creado la orden de compra: ORD"+String.valueOf(cod));        
+        }
+        catch(Exception e){
+           System.out.println(e);
+           Base.rollbackTransaction();
+        } 
+    
+    }
+    
+    private void editarPedido(OrdenCompra pedidoSeleccionado) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void refrescarTabla() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void inhabilitar_formulario() {
+        pedidoForm.setDisable(true);
+        pedidoTable.setDisable(true);
     }
     
 }
