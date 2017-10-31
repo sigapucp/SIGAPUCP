d02cc5dbdbeeb48b1b068a1010feacdf9b7e9885
Herbertvc <herbert.videla@tektonlabs.com>
2017-10-31 17:22:35 -0500

    Pyshycal Warehouse drawing

--- src/main/java/edu/pe/pucp/team_1/dp1/sigapucp/CustomComponents/RectangularDrawing.java
+++ src/main/java/edu/pe/pucp/team_1/dp1/sigapucp/CustomComponents/RectangularDrawing.java
@@ -9,6 +9,7 @@ import edu.pe.pucp.team_1.dp1.sigapucp.CustomEvents.EventArgs;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  *
@@ -31,8 +32,45 @@ public class RectangularDrawing implements Behavior {
     
     @Override
     public Boolean checkDrawRules(List<GridTile> tiles, EventArgs args) {
+        int rectangleRowStart = 0;
+        int numElementsFirstRow = 0;
+        int currentNumberOfRows = 0;
+        int previousRow = 0;
+        int currentRow = 0;
+        int lastCorner = 0;
+        int firstCorner = 0;
+        boolean indexDoesNotExist = true;
+        int indexInFirstRows = 0;
+
         Collections.sort(active_tiles);
+        firstCorner = active_tiles.get(0);
+        currentRow = firstCorner/10;
+        previousRow = currentRow;
+        rectangleRowStart = firstCorner/ 10;
+        for (Integer index : active_tiles) {
+            if(currentRow != index/10) {
+                previousRow =  currentRow;
+                currentNumberOfRows = 0;
+            };
+            currentRow = index/10;
+
+            if(currentRow == rectangleRowStart) {
+                numElementsFirstRow++;
+                lastCorner = index;
+            } else {
+                currentNumberOfRows++;
+                indexInFirstRows = index - (currentRow - rectangleRowStart)*10;
+                indexDoesNotExist = active_tiles.contains(indexInFirstRows);
+            }
+            
+            if(!indexDoesNotExist) break;
+        }
+
+        System.out.println(numElementsFirstRow);
+        System.out.println(lastCorner);
+        System.out.println(indexDoesNotExist);
         System.out.println(active_tiles);
+        
         return false;
     }
 
