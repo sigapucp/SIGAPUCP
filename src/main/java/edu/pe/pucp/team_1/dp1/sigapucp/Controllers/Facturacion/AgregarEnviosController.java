884c03cc24836531e6a9a59df5d6e13e0c0937d5
Gustavo Galarreta <gustavo.galarreta@pucp.pe>
2017-11-04 19:01:07 -0500

    orden salida crear

--- src/main/java/edu/pe/pucp/team_1/dp1/sigapucp/Controllers/Facturacion/AgregarEnviosController.java
+++ src/main/java/edu/pe/pucp/team_1/dp1/sigapucp/Controllers/Facturacion/AgregarEnviosController.java
@@ -10,14 +10,20 @@ import edu.pe.pucp.team_1.dp1.sigapucp.Models.Simulacion.Envio;
 import edu.pe.pucp.team_1.dp1.sigapucp.Navegacion.agregarEnviosArgs;
 import edu.pe.pucp.team_1.dp1.sigapucp.Navegacion.agregarOrdenCompraProductoArgs;
 import edu.pe.pucp.team_1.dp1.sigapucp.CustomEvents.Event;
+import edu.pe.pucp.team_1.dp1.sigapucp.Models.Ventas.Cliente;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import org.javalite.activejdbc.Base;
 
@@ -26,15 +32,32 @@ import org.javalite.activejdbc.Base;
  * @author Gustavo
  */
 public class AgregarEnviosController implements  Initializable{
+    //BUSQUEDA
+        //----------------------------------------------------------//
+    @FXML
+    private TextField buscar_cliente;
+    @FXML
+    private TextField buscar_envio;
+    @FXML
+    private TextField buscar_pedido;
     //TABLA
         //----------------------------------------------------------//
     @FXML
     private TableView<Envio> tabla_envios;
     @FXML
+    private TableColumn<Envio, String> columna_cod_envio;
+    @FXML
+    private TableColumn<Envio, String> columna_cliente_envio;
+    @FXML
+    private TableColumn<Envio, String> columna_pedido_envio;
+    @FXML
+    private TableColumn<Envio, String> columna_estado_envio;    
+    @FXML
     private Button boton_agregar_envio;
     //LOGICA
         //----------------------------------------------------------//
     private InformationAlertController infoController;
+    private final ObservableList<Envio> masterData = FXCollections.observableArrayList();
     private Envio envio_busqueda;
     private List<Envio> lista_envios;
     
@@ -50,8 +73,22 @@ public class AgregarEnviosController implements  Initializable{
         stage.close();        
     }
     
+    public void limpiar_tabla_envios(){
+        tabla_envios.getItems().clear();
+    }
+    
     public void llenar_tabla_envios(){
-        
+        limpiar_tabla_envios();
+        List<Envio> envios = Envio.findAll();
+        for( Envio envio : envios){
+            masterData.add(envio);
+        }
+        columna_cod_envio.setCellValueFactory((TableColumn.CellDataFeatures<Envio, String> p) -> new ReadOnlyObjectWrapper(p.getValue().get("envio_cod")));
+        columna_cliente_envio.setCellValueFactory((TableColumn.CellDataFeatures<Envio, String> p) -> new ReadOnlyObjectWrapper(Cliente.findById(p.getValue().get("client_id")).getString("nombre")));
+        columna_pedido_envio.setCellValueFactory((TableColumn.CellDataFeatures<Envio, String> p) -> new ReadOnlyObjectWrapper(p.getValue().get("orden_compra_cod")));
+        columna_estado_envio.setCellValueFactory((TableColumn.CellDataFeatures<Envio, String> p) -> new ReadOnlyObjectWrapper(p.getValue().get("estado")));
+        tabla_envios.setItems(masterData);   
+            
     }
     
     public AgregarEnviosController(){
