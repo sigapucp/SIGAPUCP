cb3ad33e9274645585b7af8e0b27dc6483caf0ab
Gustavo Galarreta <gustavo.galarreta@pucp.pe>
2017-11-07 15:18:28 -0500

    little fixes

--- src/main/java/edu/pe/pucp/team_1/dp1/sigapucp/Controllers/Facturacion/AgregarEnviosController.java
+++ src/main/java/edu/pe/pucp/team_1/dp1/sigapucp/Controllers/Facturacion/AgregarEnviosController.java
@@ -69,9 +69,8 @@ public class AgregarEnviosController implements  Initializable{
             return;            
         }
         devolverEnvioEvent.fire(this, new agregarEnviosArgs(envio_busqueda));
-        //AQUI HAY UN ERROR :S
-        //Stage stage = (Stage) boton_agregar_envio.getScene().getWindow();    
-        //stage.close();        
+        Stage stage = (Stage) boton_agregar_envio.getScene().getWindow();    
+        stage.close();        
     }
     
     public void limpiar_tabla_envios(){
